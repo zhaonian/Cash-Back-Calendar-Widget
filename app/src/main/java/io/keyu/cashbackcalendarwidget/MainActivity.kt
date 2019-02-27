cbc5dea3d3be789345f051fdecfdc5d72bb5bf32
Zhaonian Luan <lzn0830@gmail.com>
2019-02-26 21:17:03 -0800

    refactor: updated dependencies

--- app/src/main/java/io/keyu/cashbackcalendarwidget/MainActivity.kt
+++ app/src/main/java/io/keyu/cashbackcalendarwidget/MainActivity.kt
@@ -64,17 +64,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_darkmode -> {
                 // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
             }
             R.id.nav_share -> {
                 val shareIntent = Intent().apply {
